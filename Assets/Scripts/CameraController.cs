403d44a4d96c1ae3d3e4f71b982e113b8f52d23f
puthusseri <vyshakputhusseri@gmail.com>
2018-12-27 20:34:31 +0530

    Camera will follow the player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,13 +8,12 @@ public class CameraController : MonoBehaviour {
 
     private Vector3 offset;
 
-    void Start ()
-    {
+    void Start () {
+		//offset is the distance between camera and the player
         offset = transform.position - player.transform.position;
     }
-    
-    void LateUpdate ()
-    {
+    void LateUpdate () {
+		//changing the position of camera according to the offset
         transform.position = player.transform.position + offset;
     }
 }
